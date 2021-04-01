6d2be7e841cf68add289cd840ae7ccb1ce974e0d
Wilton Zanin <wiltonczanin@gmail.com>
2021-03-31 22:02:22 -0300

    Update App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import AppStack from '../MeuPossante/src/routes/AppStack'
+import AppStack from '../mobile/src/routes/AppStack'
 
 export default function App() {
   return (
