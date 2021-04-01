957ae56c66429c89f1fc569c002b9ad8f5c4f2ed
Wilton Zanin <wiltonczanin@gmail.com>
2021-03-31 21:16:56 -0300

    Alterado diretorio de pastas

---
+++ Server/src/database/migrations/1617231949287-create_usuario.ts
@@ -0,0 +1,65 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createUsuario1617231949287 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'cadastro_usuario',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'integer',
+                    unsigned: true,
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: 'nome',
+                    type: 'varchar',
+                    isNullable: false
+                },
+                {
+                    name: 'cpf',
+                    type: 'integer',
+                    length: "11",
+                    isNullable: false
+                },
+                {
+                    name: 'email',
+                    type: 'varchar',
+                    isNullable: false
+                },
+                {
+                    name: 'senha',
+                    type: 'varchar',
+                    isNullable: false
+                },
+                {
+                    name: 'celular',
+                    type: 'integer',
+                    length: "11",
+                    isNullable: false
+                },
+                {
+                    name: 'cep',
+                    type: 'integer',
+                    length: "20"
+                },
+                {
+                    name: 'data_nascimento',
+                    type: 'date'
+                },
+                {
+                    name: 'genero',
+                    type: 'varchar'
+                }
+            ],
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('cadastro_usuario')
+    }
+
+}
