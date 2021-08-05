d7ee65828b5e7d67cc5fe692396ab25a34794db9
ElisangelaWendt <61567233+ElisangelaWendt@users.noreply.github.com>
2021-08-05 20:29:18 -0300

    Alterações

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,56 @@
-import React from 'react';
-import logo from './assets/meme.png';
-import './App.css';
+import React from "react";
+import logo from "./assets/meme.png";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Olá mundo!</h1>
-      </header>
+    
+    <div className="App-header">
+      <div className="Column-Horizontal-Top">
+        <div className="FrameCarros">
+          <h1 className="Titulo">Carros</h1>
+          <div className="Column-Horizontal">
+            <div className="CardCarro"> </div>
+            <div className="Column-Horizontal">
+              <div className="CardCarro"> </div>
+            </div>
+          </div>
+        </div>
+        <div className="Column-Horizontal-Left">
+          <div className="FrameToDo">
+            <h1 className="Titulo">Carros</h1>
+            <div className="Column-Horizontal">
+              <div className="CardToDo"> </div>
+              <div className="Column-Horizontal">
+                <div className="CardToDo"> </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="Divisor"></div>
+      <div className="Column-Horizontal-Top">
+        <div className="FrameServicos">
+          <h1 className="Titulo">Carros</h1>
+          <div className="Column-Horizontal">
+            <div className="CardServicos"> </div>
+            <div className="Column-Horizontal">
+              <div className="CardServicos"> </div>
+            </div>
+          </div>
+        </div>
+        <div className="Column-Horizontal-Left">
+          <div className="FrameToDo">
+            <h1 className="Titulo">Carros</h1>
+            <div className="Column-Horizontal">
+              <div className="CardCarro"> </div>
+              <div className="Column-Horizontal">
+                <div className="CardCarro"> </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- web/src/components/navbar/navbar.tsx
+++ web/src/components/navbar/navbar.tsx
@@ -9,7 +9,7 @@ function navbar() {
   return (
     <nav id="navbar" className="navbar navbar-expand-lg navbar-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Meu Possante</a>
+        <a className="navbar-brand" href="#">Minha Mococa</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
