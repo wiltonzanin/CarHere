a28c3fd9ad46c9e168f5b372e39c4705491960a9
ElisangelaWendt <61567233+ElisangelaWendt@users.noreply.github.com>
2021-08-24 20:20:16 -0300

    Revert "Ajustes, organização e excluindo arquivos desnecessários"
    This reverts commit 5bb0d36af737440f68426f62aca897891a608d8a.

---
+++ web/src/teste.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css"; 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+ 
+function Teste() {
+  return (
+      <div>
+          teste
+      </div>
+   );
+}
+export default Teste;
