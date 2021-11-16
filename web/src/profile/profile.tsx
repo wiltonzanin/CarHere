900d9a9dca3274948825fbe2452e6b8bc8c18698
eduardobonatto <edubonattogili@hotmail.com>
2021-11-16 19:24:48 -0300

    Tela de Perfil

--- web/src/profile/profile.tsx
+++ web/src/profile/profile.tsx
@@ -7,7 +7,7 @@ function Profile() {
 
     const [avatarUrl, setAvatarUrl] = useState('');
 
-    function handleSubmit(event: FormEvent){
+    function handleSubmit(event: FormEvent) {
         event.preventDefault()
         const data = new FormData()
         data.append('avatarUrl', avatarUrl)
@@ -17,28 +17,23 @@ function Profile() {
     return (
         <div className="mainProfile">
             <Navbar />
-            {<form onSubmit={handleSubmit}> 
-            <div className="container">
-            <div className="sidebar box">
-            <img src={avatarUrl} alt="" />
-            </div>
-            <div className="conteudo box">
-            <h1 className="tituloProfile">Opções de Usuário</h1>
-            <h2 className="subtituloProfile">Perfil</h2>
-            <h3 className="nomeUsuario">Nome de usuário</h3>            
+            {<form onSubmit={handleSubmit}>
+                <div className="container">
+                    <img className="imagemPerfil" src={avatarUrl} alt="" />
+                <h1 className="tituloProfile">Opções de Usuário</h1>
+                <h2 className="subtituloProfile">Perfil</h2>
+                <h3 className="nomeUsuario">Nome de usuário</h3>
                 <input type="Name" name="Name" className="inputNomeUsuario" value="Hianna de truco" />
                 <h3 className="linkPerfil">Link da foto de perfil</h3>
                 <input type="url" name="avatar" className="inputLink" value={avatarUrl} id="avatar" placeholder="https://perfil" onChange={event => setAvatarUrl(event.target.value)} />
-            
                 <h2 className="tituloSegurança">Segurança</h2>
                 <h3 className="opçaoSegurança">Escolha uma pergunta de segurança</h3>
                 <h3 className="resposta">Resposta</h3>
-                <button type="submit">Salvar</button>
+                <button className="botao" type="submit">Salvar</button>
                 </div>
-                </div>
-            </form> }
+            </form>}
         </div>
-    
+
     )
 }
 
